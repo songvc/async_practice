961d32bf8ae2729561c2f881301c65437c0b1b71
Vincent Song <songvc@users.noreply.github.com>
2016-04-04 20:17:20 -0700

    ex4

--- exercises/ex4/ex4.js
+++ exercises/ex4/ex4.js
@@ -34,3 +34,22 @@ function getFile(file) {
 // is done.
 
 // ???
+
+
+var p1 = getFile("file1");
+var p2 = getFile("file2");
+var p3 = getFile("file3");
+
+p1
+.then(output)
+.then(function(){
+	return p2;
+})
+.then(output)
+.then(function(){
+	return p3;
+})
+.then(output)
+.then(function(){
+	output("Complete!");
+});
